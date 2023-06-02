dd82487d59ab1d12542eba4f9b21aee551a1fcb7
Raylane <raylanes315@gmail.com>
2023-06-02 19:12:58 -0300

    Atualização

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,20 +1,15 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const api = axios.create({
-    baseURL: 'https://bloggeneration.herokuapp.com'
+  baseURL: 'https://blogdothiagofaccipieri.onrender.com/'
 })
 
-    export const cadastroUsuario = async(url: any,dados: any,setDado: any) => { 
-        const resposta = await api.post(url,dados)
-        setDado(resposta.data)
-    }
+export const login = async (url: string, dados: object, setDados: any) => {
+  const resposta = await api.post(url, dados)
+  setDados(resposta.data.token)
+}
 
-    export const login = async(url: any,dados: any,setDado: any) => { 
-        const resposta = await api.post(url,dados)
-        setDado(resposta.data.token)
-    }
-
-    export const busca = async(url: any,setDado: any, header: any) => { 
-        const resposta = await api.get(url,header)
-        setDado(resposta.data)
-    }
+export const cadastrarUsuario = async (url: string, dados: object, setDados: any) => {
+  const resposta = await api.post(url, dados)
+  setDados(resposta.data)
+}
