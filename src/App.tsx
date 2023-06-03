5cd455302b517c5c8afa7e29720df618da7c10a0
Raylane <raylanes315@gmail.com>
2023-06-02 21:59:50 -0300

    Atualização

--- src/App.tsx
+++ src/App.tsx
@@ -18,38 +18,38 @@ function App() {
   return (
     <Router>
       <Navbar />
-      <div style={{ minHeight: '100vh' }}>
+    
+        <div style={{ minHeight: '100vh' }}>
         <Routes>
+        <Route path="/" element={<Login />} />
 
-          <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
 
-          <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
 
-          <Route path="/home" element={<Home />} />
+        <Route path="/cadastrousuario" element={<CadastroUsuario />} />
 
-          <Route path="/cadastrousuario" element={<CadastroUsuario />} />
+        <Route path="/temas" element={<ListaTema />} />
 
-          <Route path="/temas" element={<ListaTema />} />
+        <Route path="/posts" element={<ListaPostagem />} />
 
-          <Route path="/posts" element={<ListaPostagem />} />
+        <Route path="/formularioPostagem" element={<CadastroPost />} />
 
-          <Route path="/formularioPostagem" element={<CadastroPost />} />
+        <Route path="/formularioPostagem/:id" element={<CadastroPost />} />
 
-          <Route path="/formularioPostagem/:id" element={<CadastroPost />} />
+        <Route path="/formularioTema" element={<CadastroTema />} />
 
-          <Route path="/formularioTema" element={<CadastroTema />} />
+        <Route path="/formularioTema/:id" element={<CadastroTema />} />
 
-          <Route path="/formularioTema/:id" element={<CadastroTema />} />
+        <Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
 
-          <Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
-
-          <Route path="/deletarTema/:id" element={<DeletarTema />} />
+        <Route path="/deletarTema/:id" element={<DeletarTema />} />
 
 
         </Routes>
-      </div>
+        </div>
+     
       <Footer />
-
     </Router>
   );
 }
