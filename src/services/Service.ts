39068e48dc22927873baf61fb2e72ba9c2652ebd
Raylane <86316691+yarsico@users.noreply.github.com>
2023-06-02 22:04:59 -0300

    Update Service.ts

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,15 +1,39 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: 'https://blogdothiagofaccipieri.onrender.com/'
+    baseURL: 'https://blogpessoal-pey9.onrender.com'
 })
 
-export const login = async (url: string, dados: object, setDados: any) => {
-  const resposta = await api.post(url, dados)
-  setDados(resposta.data.token)
+export const login = async(url: string, dados: object, setDados: any) => {
+    const resposta = await api.post(url,dados)
+    setDados(resposta.data)
 }
 
-export const cadastrarUsuario = async (url: string, dados: object, setDados: any) => {
-  const resposta = await api.post(url, dados)
-  setDados(resposta.data)
+export const cadastrarUsuario = async(url: string, dados: object, setDados: any) => {
+    const resposta = await api.post(url,dados)
+    setDados(resposta.data)
+}
+
+export const getAll = async(url: any, setDados: any, headers: any) => {
+    const resposta = await api.get(url, headers)
+    setDados(resposta.data)
+}
+
+export const getById = async(url: any, setDados: any, headers: any) => {
+    const resposta = await api.get(url, headers)
+    setDados(resposta.data)
+}
+
+export const post = async(url: any, dados: object, setDados: any, headers: any) => {
+    const resposta = await api.post(url, dados, headers)
+    setDados(resposta.data)
+}
+
+export const put = async(url: any, dados: object, setDados: any, headers: any) => {
+    const resposta = await api.put(url, dados, headers)
+    setDados(resposta.data)
+}
+
+export const deleteId = async(url: any, headers: any) => {
+    await api.delete(url, headers)
 }
